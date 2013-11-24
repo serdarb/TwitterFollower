eadb78308a33577cab3f0441b71a9e76996835e1
Serdar Büyüktemiz <hserdarb@gmail.com>
2013-11-24 22:16:46 +0200

    web project created

---
+++ client/TwitterFollower.Web/Models/Tweet.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitterFollower.Web.Models
+{
+    public class Tweet
+    {
+        public string Who { get; set; }
+        public string What { get; set; }
+        public string When { get; set; }
+        public string Where { get; set; }
+        
+    }
+}
